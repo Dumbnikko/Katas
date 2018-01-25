1d31c03278ea65eac729820a6c430a8d4a0d6d06
Dumbnikko <domgrande.11@gmail.com>
2018-01-25 12:12:19 -0500

    kata

---
+++ practice/findnumber.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.argv
+
+search = '1'
+number_str = '123456789'
+
+if search in number_str:
+    print('hello')
+
+if True:
+    print("hello world")
+if False:
+    print('goodbye world')
+
+# int=> whole number
+# float=> decimal
+# boolean=> True/False
+#string=> 'word'
+
+my_variable = int('1') + 1
+my_variable = "1" + str(1)
+
+
