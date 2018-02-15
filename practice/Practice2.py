b759d52436fd67251623ac7ac617691646ded1b0
Dumbnikko <domgrande.11@gmail.com>
2018-02-15 12:45:20 -0500

    Practice Problems 2

---
+++ practice/Practice2.py
@@ -0,0 +1,69 @@
+# PRIMITIVE DATA TYPES
+# str - string
+# bool - boolean
+# int - integer
+# float
+
+# COMPLEX DATA TYPES
+# list
+# dict
+# for = will be each 'bay' in the string
+
+# CASTING
+# str + int = runtime exception
+
+# SCOPE
+# white space in Python defines scope
+#     block of code associated with a control structure
+
+# CONTROL STRUCTURES
+# --for loops--
+# for {variable_name} in <collection>:
+#     <action>
+#
+
+# --logical--
+# if <bool>:
+#   pass
+# elif <bool>:
+#   pass
+# else <bool>:
+#   pass
+#
+# --assignment--
+# =
+#
+# --comparisons--
+# == -> equals
+# != -> not equals
+# > -> greater than
+# >= -> greater than equal
+# < -> less than
+# <= -> les than equal
+
+"""
+  PRACTICE: print each letter in a given string
+"""
+grande = 'Batmon'
+
+"""
+  PRACTICE: create a function that takes an input,
+  then prints each character of the input
+"""
+
+
+def print_characters(grandez):
+    for sausage in grandez:
+        print(sausage)
+
+
+"""
+    PRACTICE: create a function that takes two inputs,
+    then prints True/False whether or not the first input
+    is contained within the second input
+"""
+
+text_value = 'some input'
+# search_string('a', text_value)  # False
+# search_string('so', text_value)  # True
+# search_string('S', text_value)  # False
